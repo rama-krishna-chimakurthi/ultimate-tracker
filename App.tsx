58d14ffffa746ae063786675a68cf42aa81ba42b
rama-krishna-chimakurthi <ramakrishnachimakurthy@gmail.com>
2024-03-09 11:39:50 +0530

    Loading database and inital Home Screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+import { Suspense, useEffect, useState } from "react";
+import { SQLiteProvider } from "expo-sqlite/next";
+import Home from "./screens/Home";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const dbName = "ultimate-tracker.db";
+const Stack = createNativeStackNavigator();
+
+const loadDatabase = async () => {
+  const dbAsset = require(`./assets/${dbName}`);
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+};
 
 export default function App() {
+  const [dbLoaded, setDbLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    loadDatabase()
+      .then(() => setDbLoaded(true))
+      .catch((e) => console.error(e));
+  }, []);
+
+  if (!dbLoaded)
+    return (
+      <View style={{ flex: 1 }}>
+        <ActivityIndicator size={"large"} />
+        <Text>Loading Database...</Text>
+      </View>
+    );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Suspense
+        fallback={
+          <View style={{ flex: 1 }}>
+            <ActivityIndicator size={"large"} />
+            <Text>Loading Database...</Text>
+          </View>
+        }
+      >
+        <SQLiteProvider databaseName={dbName} useSuspense>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                headerTitle: "Ultimate Tracker",
+                headerLargeTitle: true,
+              }}
+            />
+          </Stack.Navigator>
+        </SQLiteProvider>
+      </Suspense>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
