b9c5f321e8b9acb976656b28b3871630c202b8ac
rama-krishna-chimakurthi <ramakrishnachimakurthy@gmail.com>
2024-03-10 19:33:46 +0530

    adding cards for charts

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,14 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useSQLiteContext } from "expo-sqlite/next";
 import { SumOfTransactionsByMonth } from "../model/types";
 import { VictoryPie } from "victory-native";
+import Card from "../components/ui/Card";
 
 const CategoriesScreen = () => {
   const [categorysSummary, setCategoriesSummary] = useState<
     SumOfTransactionsByMonth[]
   >([]);
-  const [data, setData] = useState<{ x: string; y: number }[]>([]);
+  const [dataExpenses, setDataExpense] = useState<{ x: string; y: number }[]>(
+    []
+  );
+  const [dataIncomes, setDataIncome] = useState<{ x: string; y: number }[]>([]);
 
   const query = `
   SELECT 
@@ -49,23 +53,51 @@ const CategoriesScreen = () => {
     console.log(result);
     setCategoriesSummary(result);
 
-    setData([]);
-    const dataTemp: { x: string; y: number }[] = [];
-    result.forEach((category) => {
-      dataTemp.push({
-        x: category.name,
-        y: category.totalIncome + category.totalExpenses,
+    setDataExpense([]);
+    setDataIncome([]);
+    let dataTemp: { x: string; y: number }[] = [];
+    result
+      .filter((cat) => cat.totalExpenses === 0)
+      .forEach((category) => {
+        dataTemp.push({
+          x: category.name,
+          y: category.totalIncome + category.totalExpenses,
+        });
       });
-    });
     console.log(dataTemp);
-    setData(dataTemp);
-    console.log(data);
+    setDataIncome(dataTemp);
+    console.log(dataIncomes);
+
+    dataTemp = [];
+    result
+      .filter((cat) => cat.totalIncome === 0)
+      .forEach((category) => {
+        dataTemp.push({
+          x: category.name,
+          y: category.totalIncome + category.totalExpenses,
+        });
+      });
+    console.log(dataTemp);
+    setDataExpense(dataTemp);
+    console.log(dataIncomes);
   };
 
   return (
     <View>
-      <Text>CategoriesScreen</Text>
-      <VictoryPie data={data} />
+      <ScrollView>
+        <Card style={{ margin: 20, justifyContent: "center" }}>
+          <Text>Income</Text>
+          <View style={{ marginLeft: -20 }}>
+            <VictoryPie data={dataIncomes} height={300} />
+          </View>
+        </Card>
+        <Card style={{ margin: 20, justifyContent: "center" }}>
+          <Text>Expenses</Text>
+          <View style={{ marginLeft: -20 }}>
+            <VictoryPie data={dataExpenses} height={300} />
+          </View>
+        </Card>
+      </ScrollView>
     </View>
   );
 };
